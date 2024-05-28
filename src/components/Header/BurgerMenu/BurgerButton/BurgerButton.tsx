a5f37e8c7b39f0c33301837fa44104ad2768fccd
Natalia Bandurova <95983135+SplitCode@users.noreply.github.com>
2024-05-28 22:30:18 +0300

    refactor/refactor burger menu (#169)
    * refactor: refactor burger menu
    
    * fix: fix height and border
    
    * fix: fix px to rem

--- src/components/Header/BurgerMenu/BurgerButton/BurgerButton.tsx
+++ src/components/Header/BurgerMenu/BurgerButton/BurgerButton.tsx
@@ -1,12 +1,14 @@
 import styles from './BurgerButton.module.css';
-import openMenu from './../../../../assets/img/openMenu.svg';
-import closeMenu from './../../../../assets/img/closeMenu.svg';
 import type { BurgerButtonProps } from '../../Header.interfaces';
 
 export const BurgerButton = ({ toggleMenuOpen, menuOpen }: BurgerButtonProps) => {
   return (
-    <button type="button" onClick={toggleMenuOpen} className={styles.burger_button}>
-      <img src={menuOpen ? openMenu : closeMenu} alt="burger menu" />
+    <button
+      type="button"
+      onClick={toggleMenuOpen}
+      className={`${styles.burger_button} ${menuOpen ? styles.open : ''}`}
+    >
+      <span className={styles.line}></span>
     </button>
   );
 };
