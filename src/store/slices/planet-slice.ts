2ea28f762f28d8b4310713ed080bc675ec91d760
Natalia Bandurova <95983135+SplitCode@users.noreply.github.com>
2024-05-30 14:01:35 +0300

    feat/save selected planet to LS (#173)
    * feat: save choosed planet to local storage
    
    * feat: reset to default planet after logaut
    
    * fix: fix logaut order
    
    * fix: change key in ls

--- src/store/slices/planet-slice.ts
+++ src/store/slices/planet-slice.ts
@@ -7,22 +7,28 @@ export enum Planets {
   venus = 'venus',
   mars = 'mars'
 }
+
 enum PlanetsColor {
   venus = 'rgb(255,219,75)',
   earth = 'rgb(31, 163, 240)',
   mars = 'rgb(197,42,9)'
 }
+
 export interface planetSliceState {
   planet: Planets;
   accentColor: PlanetsColor;
   subcategory: Subcategories | null;
 }
 
+export const defaultPlanet: Planets = Planets.earth;
+const savedPlanet = localStorage.getItem('sloth-selectedPlanet') as Planets | null;
+
 const initialState: planetSliceState = {
-  planet: Planets.earth,
-  accentColor: PlanetsColor.earth,
+  planet: savedPlanet ?? defaultPlanet,
+  accentColor: savedPlanet ? PlanetsColor[savedPlanet] : PlanetsColor[defaultPlanet],
   subcategory: null
 };
+
 export const planetSlice = createSlice({
   name: 'planet_slice',
   initialState,
@@ -30,6 +36,7 @@ export const planetSlice = createSlice({
     setPlanet(state, action: PayloadAction<Planets>) {
       state.planet = action.payload;
       state.accentColor = PlanetsColor[action.payload];
+      localStorage.setItem('sloth-selectedPlanet', action.payload);
     },
     setSubcategory(state: planetSliceState, action) {
       state.subcategory = action.payload;
