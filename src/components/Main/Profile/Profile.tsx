3a02184184b68a72633c77103ead3925f5c703dd
Denis <131680520+DialecticalLaw@users.noreply.github.com>
2024-06-03 17:46:05 +0300

    fix/fix profile (#190)
    * fix: fix profile states
    
    * refactor: remove the transfer of an unnecessary property

--- src/components/Main/Profile/Profile.tsx
+++ src/components/Main/Profile/Profile.tsx
@@ -1,4 +1,3 @@
-import type { Address } from '@commercetools/platform-sdk';
 import { useAppSelector } from '../../../store/hooks';
 import type { CustomerSliceState } from '../../../store/slices/customer-slice';
 import styles from './Profile.module.css';
@@ -18,13 +17,10 @@ export function Profile() {
     (state) => state.customer_slice
   );
 
-  const shippingAddress = customerData?.addresses.find((address: Address) => {
-    if (customerData.shippingAddressIds) return address.id === customerData.shippingAddressIds[0];
-    return false;
-  });
-  const billingAddress = customerData?.addresses.find((address: Address) => {
-    if (customerData.billingAddressIds) return address.id === customerData.billingAddressIds[0];
-    return false;
+  const [editModes, setEditMode] = useState({
+    isPersonalEdit: false,
+    isAddressesEdit: false,
+    isPasswordEdit: false
   });
 
   if (isCustomerLoading) return <Loader />;
@@ -43,7 +39,7 @@ export function Profile() {
           <PasswordEditor setEditMode={setEditMode} customerData={customerData} />
         ) : (
           <>
-            <ProfileViewer setEditMode={setEditMode} customerData={customerData} />
+            <ProfileViewer customerData={customerData} />
             <Button
               onClick={() =>
                 setEditMode((editModes) => {
--- src/components/Main/Profile/ProfileViewer/ProfileViewer.tsx
+++ src/components/Main/Profile/ProfileViewer/ProfileViewer.tsx
@@ -2,15 +2,15 @@ import styles from './ProfileViewer.module.css';
 import avatarSrc from '../../../../assets/img/avatar.svg';
 import type { EditorProps } from '../../Main.interfaces';
 
-export function ProfileViewer({ customerData }: EditorProps) {
+export function ProfileViewer({ customerData }: Partial<EditorProps>) {
   return (
     <>
       <img src={avatarSrc} alt="avatar" className={styles.avatar} />
       <section className={styles.personal_data}>
-        <p>Имя: {customerData.firstName}</p>
-        <p>Фамилия: {customerData.lastName}</p>
-        <p>Email: {customerData.email}</p>
-        <p>Дата рождения: {customerData.dateOfBirth}</p>
+        <p>Имя: {customerData?.firstName}</p>
+        <p>Фамилия: {customerData?.lastName}</p>
+        <p>Email: {customerData?.email}</p>
+        <p>Дата рождения: {customerData?.dateOfBirth}</p>
       </section>
     </>
   );
