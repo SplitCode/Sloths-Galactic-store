5d36f4f5e22edc1dbf2335d93adfd60c8f6b0494
Valeriia Kuznetsova <122850549+ValeriiaKuz@users.noreply.github.com>
2024-06-16 15:39:29 +0300

    feat:RSS-ECOMM-4_20 (#244)
    * feat: add rss logo and year of app
    
    * refactor: make logo bigger on big screen

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -6,6 +6,9 @@ import { Accordion } from './Accordion/Accordion';
 import type { MemberData } from '../../../helpers/membersConfig';
 import { AboutUsInfo } from '../../../helpers/aboutProjectConfig';
 import { members } from '../../../helpers/membersConfig';
+import rssLogo from '../../../assets/img/rss-logo.svg';
+import { Link } from 'react-router-dom';
+
 
 export function About() {
   const [selectedMember, setSelectedMember] = useState<MemberData | null>(null);
@@ -19,19 +22,27 @@ export function About() {
   }, []);
 
   return (
-    <section className={styles.bg}>
-      <h1 className={styles.title}>О нас</h1>
-      <div className={styles.info}>
-        <Accordion data={AboutUsInfo} />
-      </div>
-      <ul className={styles.people}>
-        {members.map((member: MemberData) => (
-          <li className={styles.astronaut} key={member.name} onClick={() => memberClickHandler(member)}>
-            <img src={spaceman} alt={member.name} />
-          </li>
-        ))}
-      </ul>
-      {selectedMember && <Member member={selectedMember} onClose={closeMemberHandler} />}
-    </section>
+    <>
+      <section className={styles.bg}>
+        <h1 className={styles.title}>О нас</h1>
+        <div className={styles.info}>
+          <Accordion data={AboutUsInfo} />
+        </div>
+        <ul className={styles.people}>
+          {members.map((member: MemberData) => (
+            <li className={styles.astronaut} key={member.name} onClick={() => memberClickHandler(member)}>
+              <img src={spaceman} alt={member.name} />
+            </li>
+          ))}
+        </ul>
+        {selectedMember && <Member member={selectedMember} onClose={closeMemberHandler} />}
+      </section>
+      <footer className={styles.footer}>
+        <Link to={'https://rs.school'} target={'_blank'} className={styles.footer_link}>
+          <img src={rssLogo} alt={'RS-School-logo'} />
+        </Link>
+        <span>2024</span>
+      </footer>
+    </>
   );
 }
